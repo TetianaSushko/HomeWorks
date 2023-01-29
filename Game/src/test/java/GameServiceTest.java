f18cb6819a9259893a27ea168abf2c0089cd3942
Tanya <sushko.tn@gmail.com>
2023-01-29 13:49:35 +0200

    Home Task 16(with txt-2) with LOGGER +  resourseBundle(internationalization)

--- Game/src/test/java/GameServiceTest.java
+++ Game/src/test/java/GameServiceTest.java
@@ -1,18 +1,24 @@
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class GameServiceTest {
     @Test
     public void showWinner(){
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("Questions", Locale.GERMAN);
         GameService gameService = new GameService(1);
-        int result = gameService.checkIfYouWinner("rock", "rock");
+
+        String paper = resourceBundle.getString("paper");
+        String rock = resourceBundle.getString("rock");
+        String scissors = resourceBundle.getString("scissors");
+
+        int result = gameService.checkIfYouWinner(rock, rock, resourceBundle);
         Assert.assertEquals(0, result);
-
-        int result1 = gameService.checkIfYouWinner("paper", "rock");
+        int result1 = gameService.checkIfYouWinner(paper, rock, resourceBundle);
         Assert.assertEquals(-1, result1);
-
-
-        int result2 = gameService.checkIfYouWinner("paper", "scissors");
+        int result2 = gameService.checkIfYouWinner(paper, scissors, resourceBundle);
         Assert.assertEquals(+1, result2);
     }
 }
